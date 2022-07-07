1cac30d96eb7d8e2f996a40e2a81b374c3658b84
Saad Malik <saad_malik200@hotmail.com>
2022-07-07 13:14:20 +0200

    ex1

---
+++ solution.js
@@ -0,0 +1,6 @@
+const myArray = ["mango", 1, "apples", 300, "lunch"];
+console.log(myArray);
+const thirdElement = myArray[2];
+console.log(thirdElement);
+myArray[3] = "fourth element";
+console.log(myArray);
